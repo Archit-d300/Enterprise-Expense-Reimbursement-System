89c73d48280d0d19e2c79ee8e8457ad9c1c2efad
Archit-d300 <architdeshpande.study@gmail.com>
2026-08-24 09:56:06 +0530

    merged factory with system

--- src/factory/ExpenseFactory.java
+++ src/factory/ExpenseFactory.java
@@ -4,4 +4,23 @@ import model.ExpenseClaim;
 public abstract class ExpenseFactory {
 
     public abstract ExpenseClaim createExpense(int employeeId, double amount, String email, String description);
+
+    public static ExpenseFactory forType(String expenseType) {
+        if (expenseType == null) {
+            throw new IllegalArgumentException("Expense type must not be null.");
+        }
+
+        switch (expenseType.toUpperCase()) {
+            case "TRAVEL":
+                return new TravelExpenseFactory();
+            case "FOOD":
+                return new FoodExpenseFactory();
+            case "ACCOMMODATION":
+                return new AccommodationExpenseFactory();
+            case "OFFICE":
+                return new OfficeExpenseFactory();
+            default:
+                throw new IllegalArgumentException("Unknown expense type: " + expenseType);
+        }
+    }
 }
